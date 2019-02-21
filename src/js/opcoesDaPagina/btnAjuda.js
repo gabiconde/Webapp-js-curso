fef2f86ce0e708fa805ed192cb9a3196389a80c0
gabriela <gabiconde97@gmail.com>
2019-02-21 12:36:01 -0300

    Primeiro ajax

--- src/js/opcoesDaPagina/btnAjuda.js
+++ src/js/opcoesDaPagina/btnAjuda.js
@@ -3,6 +3,28 @@
     button.classList.remove('no-js')
 
     button.addEventListener('click', function(event){
+        
+        const req = new XMLHttpRequest()
+        req.open('GET', 'https://ceep.herokuapp.com/cartoes/instrucoes');
+        req.responseType = 'json'
+        req.send();
+
+        req.addEventListener('load', function(){
+            //const data = JSON.parse(req.response).instrucoes
+            const data = req.response.instrucoes 
+            console.log(data)
+            let cartoes = []
+
+            for(const ajuda of data){
+                cartoes.push(criaCartao(ajuda))
+            }
+/*             for(let a of data.instrucoes){
+                cartoes.push(criaCartao(a))
+            } */
+            
+        })
+/* 
+
         let lista = 
         [{
             conteudo : 'Ajuda1',
@@ -19,16 +41,12 @@
         {
             conteudo : 'Ajuda4',
             cor: 'purple'
-        }]
+        }] */
 
         /* lista.forEach((help)=>{
             console.log(help)
         }) */
-        let cartoes = []
 
-        for(let a of lista){
-            cartoes.push(criaCartao(a))
-        }
 
     })
 })()
